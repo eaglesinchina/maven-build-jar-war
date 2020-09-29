dd3fb437a937cb39bf651b3432d94d156e90a4de
eaglesinchina <eyeofeagle@126.com>
2020-09-29 11:20:47 +0800

    添加war包构建

--- src/main/java/testweb/demo/DemoApplication.java
+++ src/main/java/testweb/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package testweb.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
+    @Override
+    public SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 设置启动类,用于独立tomcat运行的入口
+        return builder.sources(DemoApplication.class);
+    }
 }
